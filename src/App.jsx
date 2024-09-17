9865b088cc0ab67e5946538dcb124003c8966998
devmfundo <devmfundo@gmail.com>
2024-09-17 23:15:58 +0200

    AuthUser component without style not connected yet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import AuthUser from './components/AuthUser';
 import Contact from './components/Contact';
 import Gallery from './components/Gallery';
 import Hero from './components/Hero';
@@ -6,10 +7,12 @@ import Portrait from './components/Portrait';
 import Footer from './Footer';
 import Header from './Header';
 
+
 const App =()=>{
   return(
     <div className='container'>
         <Header/>
+        <AuthUser/>
       <main>
         <Hero/>
         <Gallery/>
